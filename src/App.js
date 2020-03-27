72b96e3319b68e05f40f06365903b4f9a829f4f5
Hadyan Ahmad <hadyandev@gmail.com>
2020-03-27 21:34:52 +0700

    public routing with react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
 import React from "react";
-import logo from "./logo.svg";
-import "./App.css";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Registrasi from "./pages/registrasi";
+import Login from "./pages/login";
+import LupaPassword from "./pages/lupa-password";
+import NotFound from "./pages/404";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Switch>
+        <Route path="/registrasi" component={Registrasi}></Route>
+        <Route path="/login" component={Login}></Route>
+        <Route path="/lupa-password" component={LupaPassword}></Route>
+        <Route component={NotFound}></Route>
+      </Switch>
+    </Router>
   );
 }
 
